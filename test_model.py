d1134922200a9ec3f583f3f6fa1960a7fbbf703d
Srini T <32458028+srinivastelukunta@users.noreply.github.com>
2024-08-07 14:05:34 +0530

    Fixed numpy error

--- test_model.py
+++ test_model.py
@@ -1,5 +1,6 @@
 import pytest
 import pandas as pd
+import numpy as np
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
